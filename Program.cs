96c1dcf3942c33854d9e6863579a819c5fbfe41b
sRhc15 <srhc15@gmail.com>
2020-03-02 16:32:59 -0600

    Iniciando proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
